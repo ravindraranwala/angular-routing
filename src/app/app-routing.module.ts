e32b1cf5b08fb2b2457825947b1d2a603580fb73
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-07-18 13:43:38 +0530

    basic routing infrastructure was added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'customer', component: CustomerComponent },
 ];
 
 @NgModule({
